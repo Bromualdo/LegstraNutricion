7013a3529b11cda61c0aa2c714f207fa747f5710
Adrian Pozzi <adrianpozzi@gmail.com>
2023-05-26 21:07:07 -0300

    making progress

--- LegstraNutricion.js
+++ LegstraNutricion.js
@@ -1,6 +1,23 @@
-const Inicio=document.getElementById("home")
+ const Inicio=document.getElementById("home")
 
 
 Inicio.addEventListener('mouseover',()=>{
-    Inicio.style.cursor='pointer'
+    Inicio.style.backgroundColor='pointer'
 })
+
+
+function punteros(nombreDiv,nombreId){
+    nombreDiv=document.getElementById(`${nombreId}`)
+    nombreDiv.addEventListener('mouseover',()=>{
+        nombreDiv.style.cursor='pointer'
+        nombreDiv.style.backgroundColor="rgb(174, 205,173)"
+        nombreDiv.style.color="white"
+        nombreDiv.style.border="2px solid rgb(78, 165,106)"
+    })
+
+}
+
+punteros("inicio","home")
+punteros("trabajo","trabajo")
+punteros("servicios","servicios")
+punteros("modTrabajo","modTrabajo")
